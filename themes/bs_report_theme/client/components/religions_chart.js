f8dc93729cd712fe90ba02623aebd2e8f20a67b2
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-03 11:30:32 -0500

    show data

--- themes/bs_report_theme/client/components/religions_chart.js
+++ themes/bs_report_theme/client/components/religions_chart.js
@@ -10,7 +10,7 @@ export default function() {
     ready() {
       let religions = this.religions;
       let colors = this.colors;
-      console.log(this.religions);
+      console.log(this.$data);
 
       let seriesData = [];
       let subSeriesData = [];
--- themes/bs_report_theme/public/js/admin.js
+++ themes/bs_report_theme/public/js/admin.js
@@ -89,7 +89,7 @@ exports.default = function () {
         ready: function ready() {
             var religions = this.religions;
             var colors = this.colors;
-            console.log(this.religions);
+            console.log(this.$data);
 
             var seriesData = [];
             var subSeriesData = [];
