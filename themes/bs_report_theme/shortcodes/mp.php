5b4560e275fbdc422d1480dbc0d09b2c9f0d3d64
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-04 15:27:13 -0500

    change situation

--- themes/bs_report_theme/shortcodes/mp.php
+++ themes/bs_report_theme/shortcodes/mp.php
@@ -27,9 +27,8 @@
         <ul class="map__search_results">
           <li v-for="item in items">
             <a href="/report/{{item.meta_country}}">{{item.meta_country}}</a>
-
             <span>
-              <img src="<?php echo get_template_directory_uri(); ?>/public/img/icons/unchanged.svg" alt="">
+              <img src="<?php echo get_template_directory_uri(); ?>/public/img/icons/{{item.meta_situation}}.svg" alt="">
             </span>
           </li>
         </ul>
