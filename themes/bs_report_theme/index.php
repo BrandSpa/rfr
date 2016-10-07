6810429b4bcd69d7eccecc69931f9e29b62ea2e3
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-07 11:56:38 -0500

    set geoip data to json

--- themes/bs_report_theme/index.php
+++ themes/bs_report_theme/index.php
@@ -7,11 +7,16 @@
     <?php the_content() ?>
 
     <div style="padding-top: 100px"></div>
-    <?php echo json_encode(geoip_detect2_get_info_from_current_ip()) ?>
+
 
   <?php endwhile; else : ?>
     <h1><?php _e( 'Sorry, no posts matched your criteria.' ); ?></h1>
   <?php endif; ?>
 </div>
+
+<script type="text/javascript">
+  var userGeoData = JSON.parse(<?php echo json_encode(geoip_detect2_get_info_from_current_ip()) ?>);
+  console.log(userGeoData);
+</script>
 
 <?php get_footer() ?>
