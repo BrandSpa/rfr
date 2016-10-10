059cd2b173f09a249da50fc3fa566746efddd13e
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-10 15:27:37 -0500

    add transition and fix style

--- themes/bs_report_theme/single-reports.php
+++ themes/bs_report_theme/single-reports.php
@@ -109,9 +109,10 @@
 
             <div class="col-xs-6">
               <div class="report__icon-info">
-                <span class="icon-area" stlye="font-size: 51px"></span>
-                <h5 class="title-uppercase color-red"><?php echo gett('Area') ?><sup><?php echo get_post_meta($post->ID, 'report_area_sup_key', true) ?></sup></h5>
-
+                <span class="icon-area" stlyle="font-size: 51px"></span>
+                <h5 class="title-uppercase color-red">
+                  <?php echo gett('Area') ?><sup><?php echo get_post_meta($post->ID, 'report_area_sup_key', true) ?></sup>
+                </h5>
                 <p> <?php echo get_post_meta($post->ID, 'report_area_key', true) ?></p>
               </div>
             </div>
