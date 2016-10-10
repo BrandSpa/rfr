c3e477e0fc98c65741a8ed2e9f439719b063bd0a
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-10 15:24:22 -0500

    test icon with font instead of img

--- themes/bs_report_theme/single-reports.php
+++ themes/bs_report_theme/single-reports.php
@@ -109,8 +109,7 @@
 
             <div class="col-xs-6">
               <div class="report__icon-info">
-                <span class="icon-area"></span>
-                <img src="<?php echo get_template_directory_uri(); ?>/public/img/icons/area.svg" alt="">
+                <span class="icon-area" stlye="font-size: 51px"></span>
                 <h5 class="title-uppercase color-red"><?php echo gett('Area') ?><sup><?php echo get_post_meta($post->ID, 'report_area_sup_key', true) ?></sup></h5>
 
                 <p> <?php echo get_post_meta($post->ID, 'report_area_key', true) ?></p>
