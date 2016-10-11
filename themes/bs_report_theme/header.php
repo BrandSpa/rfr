a5c0370cf0e6dbedbf3ca786d87fcbb63ec2e26b
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-11 18:48:16 -0500

    remove class to burguer

--- themes/bs_report_theme/header.php
+++ themes/bs_report_theme/header.php
@@ -17,7 +17,7 @@
 </head>
 <body id="acn_reports">
 
-<a href="#" id="open-nav" class="navbar-toggle" style="position: absolute; right: 60px; top: 40px">
+<a href="#" id="open-nav" style="position: absolute; right: 60px; top: 40px">
        <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
