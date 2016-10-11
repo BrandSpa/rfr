c6ede772c76b1a9eed5b7be97cc8b5346248b5b0
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-11 18:23:19 -0500

    show the title

--- themes/bs_report_theme/templates/report/header_top.php
+++ themes/bs_report_theme/templates/report/header_top.php
@@ -12,7 +12,7 @@
   </div>
 
   <h1 class="title">
-    <?php get_the_title() ?>
+    <?php echo get_the_title() ?>
 
     <a href="#" class="open-select-countries">
       <img src="<?php echo get_template_directory_uri() . '/public/img/icons/down.svg' ?>" alt="">
