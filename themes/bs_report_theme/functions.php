08277b5f2042429c847ba437d39ea0906680f390
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-12 15:07:55 -0500

    adding again function

--- themes/bs_report_theme/functions.php
+++ themes/bs_report_theme/functions.php
@@ -8,11 +8,7 @@
 
 // enqueue the child theme stylesheet
 
-Function wp_schools_enqueue_scripts() {
-wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css'  );
-wp_enqueue_style( 'childstyle' );
-}
-add_action( 'wp_enqueue_scripts', 'wp_schools_enqueue_scripts', 11);
+
 
 
 //add navigation menus
@@ -112,6 +108,13 @@ function getLangList() {
     return _e('install polylang plugin');
   } 
 }
+
+function wp_schools_enqueue_scripts() {
+wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css'  );
+wp_enqueue_style( 'childstyle' );
+}
+
+add_action( 'wp_enqueue_scripts', 'wp_schools_enqueue_scripts', 11);
 
 
 ?>
