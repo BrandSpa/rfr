17932b94586ba66da35c750d79b5a4f82c675f0d
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-12 18:42:44 -0500

    change map

--- themes/bs_report_theme/client/components/map.js
+++ themes/bs_report_theme/client/components/map.js
@@ -73,12 +73,12 @@ export default function () {
             })
         };
 
-       var drag = d3.behavior.drag().on("drag", dragged);
+
       
       appendMap(this.mapUrl, mapContainer, () => {
         let polygons = d3.select(mapContainer).selectAll("polygon");
 
-        d3.select("#map-container svg").call(drag);
+        d3.select("#map-container svg").drag().on("drag", dragged);
         d3.select("#map-container svg g").attr('style', 'transition: all 300ms ease');
 
         let paths = d3.select(mapContainer).selectAll("path");
