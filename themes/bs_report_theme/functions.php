5bf58cbf972f1204940489c1214768904ea300af
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-12 15:10:56 -0500

    include functions from bridge

--- themes/bs_report_theme/functions.php
+++ themes/bs_report_theme/functions.php
@@ -109,6 +109,8 @@ function getLangList() {
   } 
 }
 
+include_once(get_template_directory() . '/bridge/functions.php');
+
 function wp_schools_enqueue_scripts() {
 wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css'  );
 wp_enqueue_style( 'childstyle' );
