741c70f6fd6c6b34a797df199c2c7a26713cf566
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-12 10:40:56 -0500

    add file deleted

--- themes/bs_report_theme/functions.php
+++ themes/bs_report_theme/functions.php
@@ -32,6 +32,7 @@ include_once $lib . '/get_translation.php';
 include_once $lib . '/translate_array.php';
 include_once $lib . '/translate_list.php';
 include_once $lib . '/countries.php';
+include_once $lib . '/get_reports.php';
 
 include_once $options . '/info.php';
 
