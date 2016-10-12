336e462df54a566a933899165f026cc43f625664
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-12 11:57:51 -0500

    show locale

--- themes/bs_report_theme/options/info.php
+++ themes/bs_report_theme/options/info.php
@@ -80,7 +80,7 @@ function logos_settings_page() {
 									'hide_empty' => false,
 								)); 
 								foreach($langs as $lang) {
-									echo $lang->description;
+									echo wp_parse_str($lang->locale);
 								}
 							?>
 						</ul>
