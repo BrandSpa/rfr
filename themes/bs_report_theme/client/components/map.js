6bd88c1f3f48e3e2cae04da08de395841179fb27
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-12 19:31:00 -0500

    debug event

--- themes/bs_report_theme/client/components/map.js
+++ themes/bs_report_theme/client/components/map.js
@@ -61,7 +61,7 @@ export default function () {
       let lang = this.lang;
 
       let dragged = function(d, i) {
-        console.log(d3.event.target);
+        console.log(d3.event);
           let g = document.querySelector('#map-container svg g');
           let w = g.getBBox().width / 2;
           let h = g.getBBox().height / 2;
