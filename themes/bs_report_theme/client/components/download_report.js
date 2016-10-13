0b9e25664cabe2b8136b601c8244ac2dc9312ed7
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-13 15:34:41 -0500

    debug name

--- themes/bs_report_theme/client/components/download_report.js
+++ themes/bs_report_theme/client/components/download_report.js
@@ -1,9 +1,19 @@
 'use strict';
 import Vue from 'vue';
+import validator from 'validator';
+
+const ValidatorMessages = {
+  name: 'Name required',
+  email: {
+    required: 'Email required',
+    invalid: 'Email invalid'
+  }
+};
 
 export default function() {
   Vue.component('form-download-report', {
     template: '#form-download-template',
+    
     data() {
       return {
         name: '',
@@ -16,9 +26,14 @@ export default function() {
     ready() {
      console.log(this.name);
     },
+    
     methods: {
+      validate(field) {
+        validator.isEmail('foo@bar.com');
+      },
+
       onSubmit() {
-        console.log('submit', this.$data);
+        console.log('submit', this.$data['name']);
       }
     }
   });
