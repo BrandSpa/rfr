a52a55f84daf4c9e05ae623aa1dd147dfcb0dbe2
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-13 12:06:10 -0500

    change app

--- themes/bs_report_theme/client/components/download_report.js
+++ themes/bs_report_theme/client/components/download_report.js
@@ -4,6 +4,14 @@ import Vue from 'vue';
 export default function() {
   Vue.component('form-download-report', {
     template: '#form-download-template',
+    data() {
+      return {
+        name: '',
+        email: '',
+        language: '',
+        country: ''
+      }
+    },
 
     ready() {
       console.log('hi form');
