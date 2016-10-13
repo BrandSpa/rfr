e3df869f391c30caf31058583e9c978239f995f5
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-13 10:17:28 -0500

    change meta to see it's works

--- themes/bs_report_theme/header.php
+++ themes/bs_report_theme/header.php
@@ -2,7 +2,8 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="IE=edge" http-equiv=X-UA-Compatible> 
+    <meta content="width=device-width,initial-scale=1" name=viewport>
     <title>RELIGIOUS FREEDOM REPORT - <?php wp_title(); ?></title>
     <?php wp_head(); ?>
 
