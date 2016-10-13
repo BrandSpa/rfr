176506ecf8a3d97ce85fe852dd8e094cfbed852a
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-13 10:23:00 -0500

    fix viewport

--- themes/bs_report_theme/header.php
+++ themes/bs_report_theme/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta content="IE=edge" http-equiv=X-UA-Compatible> 
-    <meta content="width=device-width,initial-scale=1" name=viewport>
+    <meta content="width=device-width,initial-scale=1" name="viewport">
     <title>RELIGIOUS FREEDOM REPORT - <?php wp_title(); ?></title>
     <?php wp_head(); ?>
 
