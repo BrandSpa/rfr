2f5a3ef994ea4aed13a51975958a97f755195fcb
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-13 10:33:09 -0500

    change viewport again

--- themes/bs_report_theme/header.php
+++ themes/bs_report_theme/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta content="IE=edge" http-equiv=X-UA-Compatible> 
-    <meta content="width=device-width,initial-scale=1" name="viewport">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
     <title>RELIGIOUS FREEDOM REPORT - <?php wp_title(); ?></title>
     <?php wp_head(); ?>
 
