db7a9db2566cbf2e23859f5b26becc704b55e31f
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-14 15:22:07 -0500

    change name options

--- themes/bs_report_theme/options/info.php
+++ themes/bs_report_theme/options/info.php
@@ -1,5 +1,6 @@
 <?php
 include_once(get_template_directory() . '/lib/countries.php');
+
 // create custom plugin settings menu
 add_action('admin_menu', 'bs_admin_options_menu');
 
@@ -46,10 +47,8 @@ function logos_settings_page() {
 
   <div style="background: #f1f1f1; background-size: contain; padding: 15px">
 		<div style="text-align: center; text-shadow: 1px 1px 3px rgba(0,0,0, .1)">
-			<h1>BS Theme Options</h1>
-			<p>Add custom info by country</p>
+			<h1>RFR Options</h1>
 		</div>
-
 
   <form method="post" action="options.php" style="position: relative; width: 80%; margin: 0 auto">
       <?php settings_fields( 'bs_country_info_group' ); ?>
