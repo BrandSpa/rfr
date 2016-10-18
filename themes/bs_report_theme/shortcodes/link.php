55116c498efba3790fdd46af9ac6058c4fb55f45
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-18 16:25:18 -0500

    fix thid svg

--- themes/bs_report_theme/shortcodes/link.php
+++ themes/bs_report_theme/shortcodes/link.php
@@ -9,39 +9,33 @@ function link_sc($atts, $content = null) {
   ob_start();
   ?>
 
-  
-<div class="link-bs">
+    <a href="<?php echo $at['link_href'] ?>" class="link-bs">
+        <svg width="14px" height="29px" viewBox="0 0 14 29" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <text id="+" stroke="none" fill="none" font-family="Roboto-Light, Roboto" font-size="24" font-weight="300">
+                <tspan x="0.214687083" y="22.2741321" fill="#FFFFFF">+</tspan>
+            </text>
+        </svg>
 
-    <a href="<?php echo $at['link_href'] ?>">
-      <svg width="14px" height="29px" viewBox="0 0 14 29" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-      <text id="+" stroke="none" fill="none" font-family="Roboto-Light, Roboto" font-size="24" font-weight="300">
-          <tspan x="0.214687083" y="22.2741321" fill="#FFFFFF">+</tspan>
-      </text>
-  </svg>
+        <svg width="14px" height="29px" viewBox="0 0 14 29" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <text id="+" stroke="none" fill="none" font-family="Roboto-Light, Roboto" font-size="24" font-weight="300">
+                <tspan x="0.214687083" y="22.2741321" fill="#FFFFFF">+</tspan>
+            </text>
+        </svg>
 
-  <svg width="14px" height="29px" viewBox="0 0 14 29" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-      <text id="+" stroke="none" fill="none" font-family="Roboto-Light, Roboto" font-size="24" font-weight="300">
-          <tspan x="0.214687083" y="22.2741321" fill="#FFFFFF">+</tspan>
-      </text>
-  </svg>
+        <?php echo gett($at['link_text']) ?> <i class="<?php echo $at['link_icon'] ?>"></i>
 
-    <?php echo gett($at['link_text']) ?> <i class="<?php echo $at['link_icon'] ?>"></i>
-       <svg width="14px" height="29px" viewBox="0 0 14 29" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <text id="+" stroke="none" fill="none" font-family="Roboto-Light, Roboto" font-size="24" font-weight="300">
-        <tspan x="0.214687083" y="22.2741321" fill="#FFFFFF">+</tspan>
-    </text>
-  </svg>
+        <svg class="link-bs__svg-third" width="14px" height="29px" viewBox="0 0 14 29" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <text id="+" stroke="none" fill="none" font-family="Roboto-Light, Roboto" font-size="24" font-weight="300">
+                <tspan x="0.214687083" y="22.2741321" fill="#FFFFFF">+</tspan>
+            </text>
+        </svg>
 
-  <svg width="14px" height="29px" viewBox="0 0 14 29" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-      <text id="+" stroke="none" fill="none" font-family="Roboto-Light, Roboto" font-size="24" font-weight="300">
-          <tspan x="0.214687083" y="22.2741321" fill="#FFFFFF">+</tspan>
-      </text>
-  </svg>
+        <svg width="14px" height="29px" viewBox="0 0 14 29" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <text id="+" stroke="none" fill="none" font-family="Roboto-Light, Roboto" font-size="24" font-weight="300">
+                <tspan x="0.214687083" y="22.2741321" fill="#FFFFFF">+</tspan>
+            </text>
+        </svg>
     </a>
-
- 
-
-</div>
 
  <?php
   return ob_get_clean();
