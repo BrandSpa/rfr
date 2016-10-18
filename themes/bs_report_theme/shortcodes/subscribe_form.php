c4dcc7a4bb4cc76ed5195d111ac741040eb4230b
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-18 10:50:57 -0500

    improve form

--- themes/bs_report_theme/shortcodes/subscribe_form.php
+++ themes/bs_report_theme/shortcodes/subscribe_form.php
@@ -6,11 +6,30 @@
   <form action="">
 
     <div class="input-container">
-      <input type="text">
+       <label for="" class="color-red">
+            <?php echo gett('Name') ?>
+            <span
+              class="input-container__error"
+              v-bind:class="{ 'input-container__error-show': validation.name}"
+            > 
+              <?php echo gett('Invalid') ?>
+            </span>
+          </label>
+
+      <input type="text" v-model="name" />
     </div>
 
     <div class="input-container">
-      <input type="text">
+        <label for="" class="color-red">
+            <?php echo gett('Email') ?>
+            <span
+              class="input-container__error"
+              v-bind:class="{ 'input-container__error-show': validation.email}"
+            > 
+              <?php echo gett('Invalid') ?>
+            </span>
+          </label>
+      <input type="text" v-model="email" />
     </div>
 
     <div class="input-container">
@@ -25,6 +44,12 @@
         <?php endforeach; ?>
       </select>
     </div>
+
+     <div class="input-container">
+          <label for="" class="color-red"><?php echo gett('Language') ?></label>
+          <select name="" id="" v-model="language"></select>
+        </div>
+
 
     <button class="button"><?php echo gett('Subscribe') ?></button>
   </form>
