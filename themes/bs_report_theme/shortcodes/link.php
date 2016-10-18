7b0a0fb12e5e6ae601d317f9cae67ac42c59a2d8
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-18 16:13:23 -0500

    test icon

--- themes/bs_report_theme/shortcodes/link.php
+++ themes/bs_report_theme/shortcodes/link.php
@@ -3,6 +3,7 @@ function link_sc($atts, $content = null) {
   $at = shortcode_atts( array(
     "link_text" => 'COUNTRY REPORTS',
     "link_href" => '#',
+    "link_icon" => '',
   ), $atts );
   
   ob_start();
@@ -24,7 +25,7 @@ function link_sc($atts, $content = null) {
       </text>
   </svg>
 
-   <?php echo gett($at['link_text']) ?>
+   <i class="icon ion-document-text"></i> <?php echo gett($at['link_text']) ?>
        <svg width="14px" height="29px" viewBox="0 0 14 29" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
     <text id="+" stroke="none" fill="none" font-family="Roboto-Light, Roboto" font-size="24" font-weight="300">
         <tspan x="0.214687083" y="22.2741321" fill="#FFFFFF">+</tspan>
@@ -67,6 +68,13 @@ function bs_link_vc() {
             "type" => "textfield",
             "heading" => "Link",
             "param_name" => "link_href",
+            "value" => ""
+          ),
+
+           array(
+            "type" => "textfield",
+            "heading" => "Link icon",
+            "param_name" => "link_icon",
             "value" => ""
           )
         
