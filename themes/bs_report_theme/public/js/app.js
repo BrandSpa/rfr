18447893f9b2056de7d116bc7ed4bfbd5f84731c
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-20 11:30:01 -0500

    get zoomIdentity

--- themes/bs_report_theme/public/js/app.js
+++ themes/bs_report_theme/public/js/app.js
@@ -20931,7 +20931,7 @@
 	      });
 
 	      d3.select('.map__controllers__more').on('click', function () {
-	        console.log(zoomed.transform);
+	        console.log(d3.zoomIdentity);
 	        svg.transition().duration(750).call(zoomed.transform, d3.zoomIdentity);
 	      });
 
