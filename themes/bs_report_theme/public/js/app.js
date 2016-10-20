a5359f31d1e3cbb07214f2b1c513ef34980e0ddc
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-20 11:19:33 -0500

    try to reset

--- themes/bs_report_theme/public/js/app.js
+++ themes/bs_report_theme/public/js/app.js
@@ -20931,7 +20931,7 @@
 	      });
 
 	      d3.select('.map__controllers__more').on('click', function () {
-	        zoomed.scaleTo(1);
+	        zoomed.scaleTo(d3.select("#map-container svg g"), 1);
 	      });
 
 	      appendMap(this.mapUrl, mapContainer, function () {
