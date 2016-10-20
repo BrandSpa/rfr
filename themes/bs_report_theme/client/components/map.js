c1934f9233441538e46c1cb2552e8adbdc6f5a08
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-20 14:30:27 -0500

    change again

--- themes/bs_report_theme/client/components/map.js
+++ themes/bs_report_theme/client/components/map.js
@@ -72,7 +72,6 @@ export default function () {
       appendMap(this.mapUrl, mapContainer, () => {
 
       d3.select('.map__controllers__more').on('click', function() {
-        console.log(d3.select("#map-container svg g"));
         d3.select("#map-container svg").transition()
         .duration(750)
         .call(zoomed.transform, d3.zoomIdentity);
--- themes/bs_report_theme/public/js/app.js
+++ themes/bs_report_theme/public/js/app.js
@@ -20931,7 +20931,6 @@
 	      appendMap(this.mapUrl, mapContainer, function () {
 
 	        d3.select('.map__controllers__more').on('click', function () {
-	          console.log(d3.select("#map-container svg g"));
 	          d3.select("#map-container svg").transition().duration(750).call(zoomed.transform, d3.zoomIdentity);
 	        });
 
