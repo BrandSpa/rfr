9b8ea4a241ceae36cb481caa70de0addaaa9751c
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-20 11:28:55 -0500

    debug zoom transform

--- themes/bs_report_theme/client/components/map.js
+++ themes/bs_report_theme/client/components/map.js
@@ -72,6 +72,7 @@ export default function () {
       });
 
       d3.select('.map__controllers__more').on('click', function() {
+        console.log(zoomed.transform);
         svg.transition()
         .duration(750)
         .call(zoomed.transform, d3.zoomIdentity);
--- themes/bs_report_theme/public/js/app.js
+++ themes/bs_report_theme/public/js/app.js
@@ -20931,6 +20931,7 @@
 	      });
 
 	      d3.select('.map__controllers__more').on('click', function () {
+	        console.log(zoomed.transform);
 	        svg.transition().duration(750).call(zoomed.transform, d3.zoomIdentity);
 	      });
 
