a4695b3e543c7407efa10e9d647f47c3367b45dc
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-20 11:34:47 -0500

    fix typos

--- themes/bs_report_theme/public/js/app.js
+++ themes/bs_report_theme/public/js/app.js
@@ -20929,7 +20929,7 @@
 	        var transform = d3.event.transform;
 	        d3.select(this).select('g').attr("transform", "translate(" + [transform.x, transform.y] + ") scale(" + transform.k + ") ");
 	      });
-	      console.log(document.querySelector("#map-container svg g∆"));
+	      console.log(document.querySelector("#map-container svg g"));
 	      d3.select('.map__controllers__more').on('click', function () {
 	        console.log(svg);
 	        svg.transition().duration(750).call(zoomed.transform, d3.zoomIdentity);
