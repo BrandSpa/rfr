4a11b29102e6eeaffe6fe5f5d75d268a08cc8d31
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-24 17:27:37 -0500

    show first

--- themes/bs_report_theme/shortcodes/home_header.php
+++ themes/bs_report_theme/shortcodes/home_header.php
@@ -10,7 +10,7 @@ function bs_home_header_sc($atts, $content = null) {
 <div class="home_header">
   <?php if(is_array( explode(',', $at['backgrounds']) )): ?>
     <?php  foreach(explode(',', $at['backgrounds']) as $key => $imageId):  ?>
-    <img class="home_header__background <?php if($key == 1) echo 'home_header__background--active' ?>" src="<?php echo wp_get_attachment_image_src($imageId, 'full')[0] ?>" alt="">
+    <img class="home_header__background <?php if($key == 0) echo 'home_header__background--active' ?>" src="<?php echo wp_get_attachment_image_src($imageId, 'full')[0] ?>" alt="">
     <?php endforeach; ?>
   <?php endif; ?>
 
