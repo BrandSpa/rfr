55e19094056db88173840ca1abc7124d98586527
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-24 15:11:47 -0500

    add to functions

--- themes/bs_report_theme/functions.php
+++ themes/bs_report_theme/functions.php
@@ -27,6 +27,7 @@ include_once $lib . '/translate_list.php';
 include_once $lib . '/countries.php';
 include_once $lib . '/get_reports.php';
 include_once $lib . '/get_user_country.php';
+include_once $lib . '/get_office_country.php';
 
 include_once $options . '/info.php';
 
