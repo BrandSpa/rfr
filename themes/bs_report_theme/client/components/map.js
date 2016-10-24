e1e19f7a0315d9499f2976cfb56f351436f284d5
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-24 16:21:48 -0500

    change size to auto

--- themes/bs_report_theme/client/components/map.js
+++ themes/bs_report_theme/client/components/map.js
@@ -71,8 +71,9 @@ export default function () {
       });
 
       appendMap(this.mapUrl, mapContainer, () => {
+        d3.select("#map-container svg").attr('height', 'auto');
         d3.select('.map__controllers__more').on('click', function() {
-          d3.select("#map-container svg").attr('height', '');
+
           d3.select("#map-container svg").transition()
           .duration(750)
           .call(zoomed.transform, d3.zoomIdentity);
--- themes/bs_report_theme/public/js/app.js
+++ themes/bs_report_theme/public/js/app.js
@@ -20930,8 +20930,9 @@
 	      });
 
 	      appendMap(this.mapUrl, mapContainer, function () {
+	        d3.select("#map-container svg").attr('height', 'auto');
 	        d3.select('.map__controllers__more').on('click', function () {
-	          d3.select("#map-container svg").attr('height', '');
+
 	          d3.select("#map-container svg").transition().duration(750).call(zoomed.transform, d3.zoomIdentity);
 	        });
 
