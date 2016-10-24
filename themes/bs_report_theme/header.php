f4d847319fe7a64e932613ab832914356b98c49f
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-24 15:18:41 -0500

    fix underscore

--- themes/bs_report_theme/header.php
+++ themes/bs_report_theme/header.php
@@ -42,8 +42,8 @@
 
     <a href="<?php echo $home ?>">
       <img src="<?php echo get_template_directory_uri(); ?>/public/img/logo.png" alt="acn logo">
-      <?php if( empty(get_option("logo_" . str_replace(' ', '-', getOfficeCountry())  )) ): ?>
-        <?php echo get_option("logo_" . str_replace(' ', '-', getOfficeCountry())  ) ?>
+      <?php if( empty( get_option("logo_" . str_replace(' ', '_', getOfficeCountry())  )) ): ?>
+        <?php echo get_option("logo_" . str_replace(' ', '_', getOfficeCountry())  ) ?>
       <?php endif; ?>
     </a>
 
