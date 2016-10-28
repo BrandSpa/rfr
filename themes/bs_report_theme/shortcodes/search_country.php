4fd561847b20b391498c6278e506a197d093a8e9
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-28 15:07:34 -0500

    show title with mo style

--- themes/bs_report_theme/shortcodes/search_country.php
+++ themes/bs_report_theme/shortcodes/search_country.php
@@ -1,7 +1,7 @@
 <template id="search-country-template">
 <div class="map__search">
   <div class="col-md-3 col-sm-10">
-    <h3 class="color-red"><?php echo gett('SEARCH BY COUNTRY') ?></h3>
+    <h1 class="title title-line color-red"><?php echo gett('SEARCH BY COUNTRY') ?></h1>
       <div class="map__search_input-container">
       <input
         type="text" 
