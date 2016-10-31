2824414d215a239465e1fdcbf6587ab658d44303
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-31 16:07:10 -0500

    feelin'

--- themes/bs_report_theme/shortcodes/search_list.php
+++ themes/bs_report_theme/shortcodes/search_list.php
@@ -12,7 +12,7 @@
     <button 
       class="bg-red map__search__list_btn" 
       data-country="Afrika" 
-      onClick="function(){ jQuery('ul[data-country="Afrika"]').style.display = 'block'  }" >Afrika</button>
+      onClick="function(){ document.querySelector('ul[data-country="Afrika"]').style.display = 'block'  }" >Afrika</button>
 
     <ul class="map__search_results" data-country="Afrika" style="display: none">
       <li
