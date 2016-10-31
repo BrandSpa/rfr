39d46a9efb5f78772ffbdc9bf430899d9e5e0512
Alejandro Sanabria <alesanabriav@gmail.com>
2016-10-31 14:52:33 -0500

    add list

--- themes/bs_report_theme/shortcodes/search_list.php
+++ themes/bs_report_theme/shortcodes/search_list.php
@@ -12,41 +12,23 @@
       <div class="map__search_input-container"></div>
 
     <ul class="map__search_results">
-    <ul>
-      <li><h3>Afrika</h3></li>
-      <li>
-        <ul>
-               <li 
-            v-for="item in continents.Afrika" 
-            v-bind:class="{'bg-discrimination': isDiscrimination(item.meta_nature_persecution), 'bg-red': isPersecution(item.meta_nature_persecution),'selected': isSelected($index) }">
-            <a href="{{item.guid}}">{{item.meta_country}}</a>
-            <span>
-              <img v-bind:src="getIcon(item.meta_situation)" alt="">
-            </span>
-          </li>
-        </ul>
-      </li>    
-    </ul>       
-  </ul>
-
-  <ul>
-    <li>
-      North Americana
-    </li>
-      <li>
-        <ul>
-          <li
-          v-for="item in continents.northAmerica" 
-          v-bind:class="{'bg-discrimination': isDiscrimination(item.meta_nature_persecution), 'bg-red': isPersecution(item.meta_nature_persecution),'selected': isSelected($index) }">
-          <a href="{{item.guid}}">{{item.meta_country}}</a>
-          <span>
-            <img v-bind:src="getIcon(item.meta_situation)" alt="">
-          </span>
-        </li>
-        </ul>
+      <li 
+        v-for="item in continents.Afrika" 
+        v-bind:class="{'bg-discrimination': isDiscrimination(item.meta_nature_persecution), 'bg-red': isPersecution(item.meta_nature_persecution),'selected': isSelected($index) }">
+        <a href="{{item.guid}}">{{item.meta_country}}</a>
+        <span>
+          <img v-bind:src="getIcon(item.meta_situation)" alt="">
+        </span>
       </li>
-       
-  </ul>
+       <li 
+        v-for="item in continents.northAmerica" 
+        v-bind:class="{'bg-discrimination': isDiscrimination(item.meta_nature_persecution), 'bg-red': isPersecution(item.meta_nature_persecution),'selected': isSelected($index) }">
+        <a href="{{item.guid}}">{{item.meta_country}}</a>
+        <span>
+          <img v-bind:src="getIcon(item.meta_situation)" alt="">
+        </span>
+      </li>
+    </ul>
 
     </div>
       <div class="col-md-3">
