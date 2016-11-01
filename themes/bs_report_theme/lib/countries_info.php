05e5f649e86bd7ac270851cc6741093b74bd1d0a
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-01 17:20:01 -0500

    fix file

--- themes/bs_report_theme/lib/countries_info.php
+++ themes/bs_report_theme/lib/countries_info.php
@@ -1,7 +1,7 @@
 <?php
 
   function getCountriesInfo() {
-    $json = file_get_contents('countries_info.json');
+    $json = file_get_contents(__DIR__ . '/countries_info.json');
     return json_decode($json, true);
   }
 
