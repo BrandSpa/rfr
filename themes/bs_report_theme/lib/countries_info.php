2a1a22043e9b647ddfb3fa23fabd40de539196e5
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-01 17:30:23 -0500

    fix array

--- themes/bs_report_theme/lib/countries_info.php
+++ themes/bs_report_theme/lib/countries_info.php
@@ -5,7 +5,7 @@
     $name = 'Colombia';
     $json = file_get_contents(__DIR__ . '/countries_info.json');
     $countries = json_decode($json, true);
-    return array_filter($countrie, function($arr)  use($name) {
+    return array_filter($countries, function($arr) use($name) {
       return $arr['name'] == $name;
     }
   );
