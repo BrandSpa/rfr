9895da3d06f649ac7b144684d3ba029f85f5df33
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-01 14:56:19 -0500

    fix float

--- themes/bs_report_theme/shortcodes/press.php
+++ themes/bs_report_theme/shortcodes/press.php
@@ -1,3 +1,3 @@
-<div style="background: #fff; padding: 120px 40px;">
+<div style="background: #fff; padding: 120px 40px; width: 100%; float:left">
   <h1 class="title-center title-line color-red"> <?php echo gett('PRESS'); ?> </h1>
 </div>
