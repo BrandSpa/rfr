43d9c139cbadbd480955c350c8794b19617ddb20
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-02 15:54:41 -0500

    set cookie

--- themes/bs_report_theme/index.php
+++ themes/bs_report_theme/index.php
@@ -9,12 +9,7 @@
   if($uri == '/' || $uri == '' || empty($uri)) {
     $lang = getCountriesInfo(getUserCountry())->languages[0];
     $url = "http://".$_SERVER['HTTP_HOST'].'/'.$lang;
-    setcookie("lang", $lang);
-    
-    header("Location:" . $url); /* Redirect browser */
-
-    /* Make sure that code below does not get executed when we redirect. */
-    exit;
+    setcookie("nea", $lang);
   }
   
 ?>
