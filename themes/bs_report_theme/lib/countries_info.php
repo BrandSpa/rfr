4614d02e4517572da9be2e1c2ae5fbecdc2c6b4a
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-02 15:26:02 -0500

    get by iso

--- themes/bs_report_theme/lib/countries_info.php
+++ themes/bs_report_theme/lib/countries_info.php
@@ -1,12 +1,12 @@
 <?php
     
-  function getCountriesInfo($name = 'United States') {
+  function getCountriesInfo($name = 'US') {
     $json = file_get_contents(__DIR__ . '/countries_info.json');
     
     $countries = json_decode($json);
 
     $country = array_filter($countries, function($arr) use($name) {
-      return $arr->name == $name;
+      return $arr->iso_code == $name;
     });
 
     return $country[array_keys($country)[0]];
