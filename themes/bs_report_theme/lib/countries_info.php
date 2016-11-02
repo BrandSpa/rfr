9907ae83fecb5da101aa6e7e3ab6aae17c90dbb3
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-02 10:49:27 -0500

    try json decode without associative

--- themes/bs_report_theme/lib/countries_info.php
+++ themes/bs_report_theme/lib/countries_info.php
@@ -4,15 +4,13 @@
   function getCountriesInfo($name = 'United States') {
     $json = file_get_contents(__DIR__ . '/countries_info.json');
     
-    $countries = json_decode($json, true);
+    $countries = json_decode($json);
 
     $country = array_filter($countries, function($arr) use($name) {
       return $arr['name'] == $name;
     });
 
     return $country;
-
-
   }
 
 ?>
