faf2437874bc65b710335470fd516e26bfd3f8fd
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-02 17:37:49 -0500

    set function on functions

--- themes/bs_report_theme/index.php
+++ themes/bs_report_theme/index.php
@@ -1,11 +1,5 @@
 <?php
-  $uri = $_SERVER['REQUEST_URI'];
-  $iso = getUserCountry()->isoCode;
-  $lang = getCountriesInfo($iso)->languages[0];
-  unset($_COOKIE["pll_language"]);
-  print_r($_COOKIE["pll_language"]);
-  setcookie("pll_language", $lang);
-  print_r(isset(pll_the_languages( array( 'raw' => 1 ) )[$lang]) && is_array(pll_the_languages( array( 'raw' => 1 ) )[$lang]));
+  
 ?>
 
 <?php get_header() ?>
