c2f2145037adbb254b4813cedae5d583e6b3ffa3
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-03 14:18:18 -0500

    remove button special

--- themes/bs_report_theme/shortcodes/download_report.php
+++ themes/bs_report_theme/shortcodes/download_report.php
@@ -4,7 +4,7 @@
   <h1 class="title-center title-line color-red">
     <?php echo gett('DOWNLOAD REPORT'); ?>
   </h1>
-
+ 
   <div class="row">
     <div class="col-md-1"></div>
     <div class="col-md-5">
@@ -24,37 +24,7 @@
       </span>
 
       </a>
-      <a href="#" class="button-special">
-        <svg width="48px" height="62px" viewBox="350 6238 48 62" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" >
-
-            <g id="Group-20" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" transform="translate(350.000000, 6238.000000)">
-                <path d="M0,0 L0,61.333232 L47.0298293,61.333232 L47.0298293,8.54142696 L38.3140414,0 L0,0 Z M4.27543902,4.33669317 L37.0191942,4.33669317 L42.7543902,9.7141927 L42.7543902,56.9965388 L4.27543902,56.9965388 L4.27543902,4.33669317 Z" id="Fill-1" fill="#FC3938"></path>
-                <polygon id="Fill-2" fill="#FC3938" points="34.0956159 6.29550119 34.0956159 12.8631132 40.5344271 12.8631132 40.5344271 10.6743222 36.2431079 10.6743222 36.2431079 6.29550119"></polygon>
-                <polygon id="Fill-3" fill="#FC3938" points="10.3769266 25.798779 36.1291174 25.798779 36.1291174 23.6081295 10.3769266 23.6081295"></polygon>
-                <polygon id="Fill-4" fill="#FC3938" points="10.3769266 36.8152866 36.1291174 36.8152866 36.1291174 34.6252565 10.3769266 34.6252565"></polygon>
-                <polygon id="Fill-5" fill="#FC3938" points="10.3769266 47.8305551 36.1291174 47.8305551 36.1291174 45.6423836 10.3769266 45.6423836"></polygon>
-            </g>
-        </svg>
-        <span>
-          <?php echo gett('Full Report') ?> <i><?php echo gett('Spanish') ?></i>
-        </span>
-      </a>
-
-      <a href="#" class="button-special">
-        <svg width="48px" height="62px" viewBox="350 6238 48 62" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" >
-
-            <g id="Group-20" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" transform="translate(350.000000, 6238.000000)">
-                <path d="M0,0 L0,61.333232 L47.0298293,61.333232 L47.0298293,8.54142696 L38.3140414,0 L0,0 Z M4.27543902,4.33669317 L37.0191942,4.33669317 L42.7543902,9.7141927 L42.7543902,56.9965388 L4.27543902,56.9965388 L4.27543902,4.33669317 Z" id="Fill-1" fill="#FC3938"></path>
-                <polygon id="Fill-2" fill="#FC3938" points="34.0956159 6.29550119 34.0956159 12.8631132 40.5344271 12.8631132 40.5344271 10.6743222 36.2431079 10.6743222 36.2431079 6.29550119"></polygon>
-                <polygon id="Fill-3" fill="#FC3938" points="10.3769266 25.798779 36.1291174 25.798779 36.1291174 23.6081295 10.3769266 23.6081295"></polygon>
-                <polygon id="Fill-4" fill="#FC3938" points="10.3769266 36.8152866 36.1291174 36.8152866 36.1291174 34.6252565 10.3769266 34.6252565"></polygon>
-                <polygon id="Fill-5" fill="#FC3938" points="10.3769266 47.8305551 36.1291174 47.8305551 36.1291174 45.6423836 10.3769266 45.6423836"></polygon>
-            </g>
-        </svg>
-        <span>
-          <?php echo gett('Full Report') ?> <i><?php echo gett('Italian') ?></i>
-        </span>
-      </a>
+    
     </div>
     <div class="col-md-5">
       <form-download-report country="<?php echo getUserCountry() ?>"></form-download-report>
