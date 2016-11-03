6689a6c19161976844dbde0db9e25c7e8645ba7d
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-03 11:10:03 -0500

    filter empty

--- themes/bs_report_theme/client/components/search_list.js
+++ themes/bs_report_theme/client/components/search_list.js
@@ -21,9 +21,11 @@ export default function() {
         if(reports.filter(r => r.meta_country == coun)[0]) {
           return reports.filter(r => r.meta_country == coun)[0];
         } else {
-          return {};
+          return '';
         }
       };
+
+      filterByCountry = filterByCountry.filter(report => typeof report == 'object');
 
       newContinents['afrika'] = continents['Afrika'].map(filterByCountry);
 
