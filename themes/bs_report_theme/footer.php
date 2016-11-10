9bb7e781f9cf50710952e8486573533ff63a1dfb
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-10 11:21:57 -0500

    remove script

--- themes/bs_report_theme/footer.php
+++ themes/bs_report_theme/footer.php
@@ -39,10 +39,7 @@
   <script src="http://code.highcharts.com/modules/exporting.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/flexibility/2.0.1/flexibility.js"></script>
   <script src="<?php echo get_template_directory_uri() . '/public/js/app.js' ?>"></script>
-  <script>
-    flexibility(document.getElementsByClassName('report__header_pdf'));
-  </script>
-
+ 
 <?php wp_footer() ?>
 
 </body>
