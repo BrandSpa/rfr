eaca7df01df68269429b878a4184487fcc3d53f6
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-11 17:52:55 -0500

    gett

--- themes/bs_report_theme/shortcodes/mp.php
+++ themes/bs_report_theme/shortcodes/mp.php
@@ -25,7 +25,7 @@
         <h1 class="title title-line color-red">
           <?php echo gett('BY COUNTRY'); ?>
         </h1>
-        <h4 style="color: #fff">Click on any country to see its report.</h4>
+        <h4 style="color: #fff"><?php echo gett('Click on any country to see its report.') ?></h4>
       </div>
       
       <div class="col-md-4">
