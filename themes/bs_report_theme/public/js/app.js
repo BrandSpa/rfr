22180e0fc312128a66083cec0573c9854bc457d5
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-11 12:15:13 -0500

    get hash

--- themes/bs_report_theme/public/js/app.js
+++ themes/bs_report_theme/public/js/app.js
@@ -127,24 +127,24 @@
 	  (0, _jquery2.default)("a").on('click', function (event) {
 	    console.log(this.hash);
 
-	    // Make sure this.hash has a value before overriding default behavior
-	    if (this.hash !== "") {
-	      // Prevent default anchor click behavior
-	      event.preventDefault();
+	    // // Make sure this.hash has a value before overriding default behavior
+	    // if (this.hash !== "") {
+	    //   // Prevent default anchor click behavior
+	    //   event.preventDefault();
 
-	      // Store hash
-	      var hash = this.hash;
+	    //   // Store hash
+	    //   var hash = this.hash;
 
-	      // Using jQuery's animate() method to add smooth page scroll
-	      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-	      (0, _jquery2.default)('html, body').animate({
-	        scrollTop: (0, _jquery2.default)(hash).offset().top
-	      }, 800, function () {
+	    //   // Using jQuery's animate() method to add smooth page scroll
+	    //   // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+	    //   $('html, body').animate({
+	    //     scrollTop: $(hash).offset().top
+	    //   }, 800, function(){
 
-	        // Add hash (#) to URL when done scrolling (default click behavior)
-	        window.location.hash = hash;
-	      });
-	    } // End if
+	    //     // Add hash (#) to URL when done scrolling (default click behavior)
+	    //     window.location.hash = hash;
+	    //   });
+	    // } // End if
 	  });
 	});
 
