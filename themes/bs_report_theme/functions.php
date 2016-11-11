1c86151867c9074bb93dacd80c6f96949b319c0a
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-11 16:55:05 -0500

    activate shortcodes

--- themes/bs_report_theme/functions.php
+++ themes/bs_report_theme/functions.php
@@ -22,7 +22,7 @@ include'lib/index.php';
 include 'apis/index.php';
 // include 'options/index.php';
 // include 'metaboxes/index.php';
-// include 'shortcodes/index.php';
+include 'shortcodes/index.php';
 
 setTranslations();
 
