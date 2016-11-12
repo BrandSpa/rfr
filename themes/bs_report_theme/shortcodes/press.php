343baec3c32d1f363f94f115ee1ac83ed901c3a7
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-11 21:53:35 -0500

    change padding

--- themes/bs_report_theme/shortcodes/press.php
+++ themes/bs_report_theme/shortcodes/press.php
@@ -1,4 +1,4 @@
-<div style="background: #fff; padding: 120px 40px; width: 100%; float:left">
+<div style="background: #fff; padding: 60px 40px; width: 100%; float:left">
   <h1 class="title-center title-line color-red"> <?php echo gett('PRESS'); ?> </h1>
   <div style="padding-bottom: 60px"></div>
   <div class="col-md-1"></div>
