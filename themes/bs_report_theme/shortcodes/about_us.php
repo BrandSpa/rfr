1b0b0d465f374623e5ef9ef274e7353f7f2c01a1
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-11 21:52:30 -0500

    severa ginza

--- themes/bs_report_theme/shortcodes/about_us.php
+++ themes/bs_report_theme/shortcodes/about_us.php
@@ -4,6 +4,7 @@
   <h1 class="title-center title-line color-red">
     <?php echo gett('ABOUT US'); ?>
   </h1>
+  <div style="padding-bottom: 60px"></div>
   <div class="row">
     <div class="col-md-1"></div>
 
