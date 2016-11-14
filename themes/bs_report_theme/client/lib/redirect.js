dde8f412d1c6564fdbe9b81744a4341943190fa8
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-13 21:53:13 -0500

    get country

--- themes/bs_report_theme/client/lib/redirect.js
+++ themes/bs_report_theme/client/lib/redirect.js
@@ -2,6 +2,6 @@ const countries = require("json!./countries_list.json");
 
 export default function redirect() {
   let country = countries.filter(country => country.name.toLowerCase() == bs.geo.country.names.en.toLowerCase() );
-  console.log(country.languages);
+  console.log(country);
   console.log(bs.geo.country.names.en);
 }
