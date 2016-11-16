64b19fe2755d8839c24148fc51f02591cb5e47d0
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-16 12:07:28 -0500

    change slider

--- themes/bs_report_theme/shortcodes/slider.php
+++ themes/bs_report_theme/shortcodes/slider.php
@@ -32,11 +32,14 @@ function bs_slider_sc($atts, $content = null) {
 <?php endif; ?>
 
 <script>
-  (function() {
-    var $ = jQuery;
-
-
-  })();
+  $(function() {
+    slider({
+      sliderId: <?php echo $id?>, 
+      itemWidth: <?php echo $at['image_width'] ?>,
+      itemMargin: <?php echo $at['item_margin'] ?>,
+      controlNav: <?php echo $at['control_nav'] ?>
+    });
+  });
 </script>
 
 <?php
