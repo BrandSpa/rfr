17629ba1b0760643fe713fe445473974ab73f373
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-16 15:57:20 -0500

    fix script

--- themes/bs_report_theme/footer.php
+++ themes/bs_report_theme/footer.php
@@ -64,11 +64,11 @@
     var bs = {};
     bs['geo'] = JSON.parse(JSON.stringify(<?php echo json_encode(geoip_detect2_get_info_from_current_ip()); ?>));
     bs['langs'] = JSON.parse(JSON.stringify(<?php echo json_encode(pll_the_languages(array('raw'=>1))) ?>));
-    bs.slider = function(params = {}) {
+    
   </script>
 
 	<script type="text/babel">
-		    
+		bs.slider = function(params = {}) {
 		const {sliderId, imageWidth, itemMargin, controlNav} = params;
       
       $(() => {
@@ -82,8 +82,7 @@
         });
 	  });
 
-  }
-
+	}
 	</script>
 
   <script src="<?php echo get_template_directory_uri() . '/public/js/app.js' ?>"></script>
