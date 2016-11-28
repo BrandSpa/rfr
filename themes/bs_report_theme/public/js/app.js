c3a0f428990e4093b1bf0e6fca6aa5e4045459c3
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-28 17:44:10 -0500

    this

--- themes/bs_report_theme/public/js/app.js
+++ themes/bs_report_theme/public/js/app.js
@@ -29176,6 +29176,8 @@
 	        return false;
 	      },
 	      search: function search(evt) {
+	        var _this4 = this;
+
 	        var reports = this.reports;
 	        var query = this.country;
 	        var results = [];
@@ -29187,7 +29189,7 @@
 	        if (query.length > 0) {
 
 	          results = reports.filter(function (pst) {
-	            var translation = countriesTrans(pst.meta_country);
+	            var translation = _this4.countriesTrans(pst.meta_country);
 	            return translation.toLowerCase().indexOf(query.toLowerCase()) != -1;
 	          });
 	        } else {
