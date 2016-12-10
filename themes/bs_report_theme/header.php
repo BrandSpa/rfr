9498563c399202b7067d8e01cb0a2ee67f4cd712
Alejandro Sanabria <alesanabriav@gmail.com>
2016-12-10 15:49:04 -0500

    office

--- themes/bs_report_theme/header.php
+++ themes/bs_report_theme/header.php
@@ -42,7 +42,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 
 <div class="hidden">
-<?php echo  get_option('office_redirect_' . str_replace(' ', '_', getOfficeCountry()) ); ?>
+<?php echo 'office: '. get_option('office_redirect_' . str_replace(' ', '_', getOfficeCountry()) ); ?>
 </div>
 <body id="acn_reports">
 
