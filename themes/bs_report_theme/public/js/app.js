95f5b169d1412350fd26004eef5bdd4b5aa9af2b
Alejandro Sanabria <alesanabriav@gmail.com>
2017-01-11 10:50:29 -0500

    come on works

--- themes/bs_report_theme/public/js/app.js
+++ themes/bs_report_theme/public/js/app.js
@@ -11657,8 +11657,8 @@
 	            d3.select(this).on("mousemove", function (e) {
 	              var box = d3.select("#map-container svg g").node().getBBox();
 	              var w = box.width / 2;
-	              transformY = d3.event.clientY - elOffsetTop - startX;
-	              transformX = d3.event.clientX - elOffsetLeft - startY;
+	              transformY = transformY + d3.event.clientY - startY; //(d3.event.clientY - elOffsetTop) - startX
+	              transformX = transformX + d3.event.clientX - startX;
 	              d3.select(_this2).select('g').attr("transform", 'translate(' + transformX + ',' + transformY + ') scale(' + transformScale + ')');
 	            });
 
