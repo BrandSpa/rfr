15527e19f1c1f10956215a0109523e7ae077f895
Alejandro Sanabria <alesanabriav@gmail.com>
2017-01-25 16:04:48 -0500

    get meta posts

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php get_header() ?>
+<div class="hidden">
+  <?php echo get_post_meta($post->ID,'page_redirect_url_key', true) ?>
+  <?php echo get_post_meta($post->ID, 'page_redirect_country_key', true) ?>
+</div>
 
 <div >
 
@@ -17,7 +21,5 @@
 
 <?php include_once 'shortcodes/about_us.php' ?>
 <?php include_once 'shortcodes/press.php' ?>
- 
-
 
 <?php get_footer() ?>
