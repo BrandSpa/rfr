b7576a8b65efd36bf8b81ec2cf3d7c65b8671183
Alejandro Sanabria <alesanabriav@gmail.com>
2017-01-25 16:08:52 -0500

    get country name

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php get_header() ?>
 <div class="hidden">
-  <?php echo geoip_detect2_get_info_from_current_ip(); ?>
+  <?php echo geoip_detect2_get_info_from_current_ip()->country->names['en']; ?>
   <?php echo get_post_meta($post->ID,'page_redirect_url_key', true) ?>
   <?php echo get_post_meta($post->ID, 'page_redirect_country_key', true) ?>
   
