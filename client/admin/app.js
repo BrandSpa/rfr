9355d9b560b22b8504d2fa58236c083a1a7ad05e
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-17 14:16:06 -0500

    get highcharts

--- client/admin/app.js
+++ client/admin/app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+import multipleRender from 'react-multiple-render'; 
 import uploader from './uploader';
 // import religionsChart from '../components/religions_chart';
 import ReligionsChart from '../components-react/religionsChart';
