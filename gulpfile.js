5971f7437ef145e9e936d9eb81e6abee5695e5eb
Alejandro Sanabria <alesanabriav@gmail.com>
2017-03-23 09:53:18 -0500

    remove gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,34 +2,10 @@
 const fs = require('fs');
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const babel = require('gulp-babel');
-const browserify = require("browserify");
-const babelify = require("babelify");
 const autoprefixer = require('gulp-autoprefixer');
 const uglify = require('gulp-uglify');
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
-
-//npm install -S fs gulp gulp-sass gulp-babel browserify babel babel-preset-es2015 babelify gulp-autoprefixer gulp-uglify vinyl-source-stream vinyl-buffer
-gulp.task('admin', () => {
-  browserify("./client/admin/app.js")
-    .transform("babelify", {presets: ["es2015"]})
-    .bundle()
-    .pipe(fs.createWriteStream("public/js/admin.js"));
-});
-
-gulp.task('app', () => {
-  browserify("./client/app.js")
-    .transform("babelify", {presets: ["es2015"]})
-    .bundle()
-    .on('error', function(err) {
-      console.log(err);
-    })
-    .pipe(source('app.js'))
-    // .pipe(buffer())
-    // .pipe(uglify())
-    .pipe(gulp.dest('public/js'));
-});
 
 gulp.task('sass', function () {
   return gulp.src('./sass/main.scss')
@@ -39,14 +15,6 @@ gulp.task('sass', function () {
 			cascade: false
 		}))
     .pipe(gulp.dest('./public/css'));
-});
-
-gulp.task('admin:watch', function () {
-  gulp.watch('./client/admin/**/*.js', ['admin']);
-});
-
-gulp.task('app:watch', function () {
-  gulp.watch('./client/**/*.js', ['app']);
 });
 
 gulp.task('sass:watch', function () {
