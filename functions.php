9cb2ce4ee8202e6671690ac34c02e2ba036e4963
Michael Sanchez <michaelsanchez@MacBook-Air-de-Michael.local>
2018-01-29 12:10:23 -0500

    remove redaclare getCountryLang

--- functions.php
+++ functions.php
@@ -135,22 +135,6 @@ if(!function_exists('getCountry')) {
   }
 }
 
-function getCountryLang($name) {
-    $json = file_get_contents(__DIR__ . '/countries_lang.json');
-    $countries = json_decode($json);
-
-    $country = array_filter($countries, function($arr) use($name) {
-      return $arr->name == $name;
-    });
-
-		if(array_keys($country)) {
-			return $country[array_keys($country)[0]] ? $country[array_keys($country)[0]]->languages[0] : 'en';
-		} else {
-			return 'en';
-		}
-	
-}
-
 function redirectToLang() {
 	$lang = getCountryLang(getCountry());
 	if(!isset($_COOKIE['bs-lang']) && empty($_COOKIE['bs-lang'])) {
